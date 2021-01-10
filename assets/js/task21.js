b5b099b0f9ac8ffb6aff1d1706c77b103242059e
julietvt <freshitpt@gmail.com>
2021-01-10 12:12:09 +0200

    super for parent method

--- assets/js/task21.js
+++ assets/js/task21.js
@@ -27,3 +27,37 @@ const animal1 = new Animal('SomeAnimal');
 const cat1 = new Cat('Barsik', 8);
 cat1.run();
 cat1.sound();
+
+// super может вызывать конструктор родителя и его методы (родителя)
+class A {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  getPoint() {
+    return this.x + ' ' + this.y;
+  }
+}
+class D extends A {
+  constructor(x, y, z) {
+    super(x, y);
+    this.z = z;
+  }
+  getPoint() {
+    return this.x + ' ' + this.y + ' ' + this.z;
+  }
+  getPointPatternParent() {
+    // вызов метода getPoint у родителя
+    return super.getPoint();
+  }
+}
+
+const p1 = new A(2, 3);
+
+const p2 = new D(4, 5, 6);
+
+p1.getPoint();
+
+p2.getPoint();
+
+p2.getPointPatternParent();

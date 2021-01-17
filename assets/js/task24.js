eabcaefb96366c1b9fe91eaab957a0a5206fdbbf
julietvt <freshitpt@gmail.com>
2021-01-17 12:37:32 +0200

    add list iterator

--- assets/js/task24.js
+++ assets/js/task24.js
@@ -34,7 +34,7 @@ class Stack {
     if (this._size >= this._maxSize) {
       throw new RangeError('Stack is overflow');
     }
-    this[this._size++] = value;
+    this[`_${this._size++}`] = value;
     return this._size;
   }
   // удалил и вернут элемент
@@ -52,6 +52,10 @@ class Stack {
       return;
     }
     return this[this._size - 1];
+  }
+
+  [Symbol.iterator]() {
+    return new StackIterator(this);
   }
 }
 
@@ -94,3 +98,116 @@ function lightCheck(
   }
   return bracketsStack.length == 0;
 }
+
+// Пусть есть словарик. сделать перевод
+
+const vocabulary = new Map();
+vocabulary.set('cat', 'кот');
+vocabulary.set('dog', 'собака');
+vocabulary.set('squirrel', 'белка');
+vocabulary.set('create', 'создать');
+vocabulary.set('read', 'читать');
+vocabulary.set('update', 'обновить');
+vocabulary.set('delete', 'удалить');
+
+const translate = (str) =>
+  str
+    .split(' ')
+    .map((w) => vocabulary.get(w))
+    .join(' ');
+
+const str = 'cat dog read squirrel';
+console.log(translate(str));
+
+// stack + iterator
+
+class StackIterator {
+  constructor(stack) {
+    this._stack = stack;
+    this._start = 0;
+  }
+  next() {
+    if (this._start >= this._stack.size) {
+      return {
+        value: undefined,
+        done: true,
+      };
+    }
+    return {
+      value: this._stack[`_${this._start}`],
+      //value: this.stack[this.start],
+      done: this._start++ === this._stack.size,
+    };
+  }
+}
+
+const stack2 = new Stack();
+for (let i = 0; i < 100; i++) {
+  stack2.push(i * (Math.random() * 100));
+}
+const array2 = [...stack2];
+
+// список
+
+class ListItem {
+  constructor(value, next = null, prev = null) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class List {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+  addBegin(value) {
+    const newHead = new ListItem(value, this.head, null);
+    if (this.head) {
+      this.head.prev = newHead;
+    }
+    this.head = newHead;
+    if (!this.tail) {
+      this.tail = newHead;
+    }
+    this.size++;
+  }
+  addEnd(value) {
+    const newTail = new ListItem(value, null, this.tail);
+    if (this.tail) {
+      this.tail.next = newTail;
+    }
+    this.tail = newTail;
+    if (!this.head) {
+      this.head = newTail;
+    }
+    this.size++;
+  }
+  [Symbol.iterator]() {
+    let current = this.head;
+    return {
+      next() {
+        if (current) {
+          const value = current.value;
+          current = current.next;
+          return {
+            value: value,
+            done: false,
+          };
+        } else {
+          return {
+            done: true,
+          };
+        }
+      },
+    };
+  }
+}
+
+const list = new List();
+list.addEnd('Tom');
+list.addEnd(100);
+list.addEnd({ stud: { name: 'John Smith' } });
+console.log(list);

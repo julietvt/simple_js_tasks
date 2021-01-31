956d41a861ab689c7f1dd832743610b2d7b9e757
julietvt <freshitpt@gmail.com>
2021-01-31 13:27:38 +0200

    dom event: change color

--- assets/js/task28.js
+++ assets/js/task28.js
@@ -1,3 +1,4 @@
+/*
 var rect = document.getElementById('rect');
 
 rect.addEventListener('click', handlerFun2);
@@ -31,8 +32,26 @@ function handlerFun2(e) {
   }
 }
 
-/* Event
-target: указывает на элемент, на котором было вызвано событие
-
-type: указывает на имя события
+// Event
+//target: указывает на элемент, на котором было вызвано событие
+//type: указывает на имя события
 */
+
+const blocks = document.querySelectorAll('.changeColor');
+
+//1 Задание по клику изменить цвет
+
+for (let elem of blocks) {
+  elem.addEventListener('click', funChangeColor1);
+}
+
+function funChangeColor1(e) {
+  e.target.style.backgroundColor = 'yellow';
+  e.target.addEventListener('click', funChangeColor2);
+}
+
+function funChangeColor2(e) {
+  e.target.style.backgroundColor = 'purple';
+  e.target.removeEventListener('click', funChangeColor2);
+  e.target.addEventListener('click', funChangeColor1);
+}

b20f7d4e2383b1b75c2480bdec6b5f91ddc4d91c
julietvt <freshitpt@gmail.com>
2021-02-06 11:07:31 +0200

    add simply slider actions

--- assets/js/task29.js
+++ assets/js/task29.js
@@ -5,3 +5,27 @@ document.getElementById('id1').onclick = function () {
   button_id2.hidden = true;
 };
 */
+let slider = document.querySelector('.slider');
+let thumb = document.querySelector('.thumb');
+thumb.onmousedown = function (event) {
+  event.preventDefault();
+  let newShiftX = event.clientX - thumb.getBoundingClientRect().left;
+  document.addEventListener('mousemove', onMouseMoveAction);
+  document.addEventListener('mouseup', onMouseUpAction);
+
+  function onMouseMoveAction(event) {
+    // new left position
+    let newLeftPosition =
+      event.clientX - newShiftX - slider.getBoundingClientRect().left;
+    if (newLeftPosition < 0) newLeftPosition = 0;
+    let rightPosition = slider.offsetWidth - thumb.offsetWidth;
+    if (newLeftPosition > rightPosition) newLeftPosition = rightPosition;
+    thumb.style.left = newLeftPosition + 'px';
+    console.log('left:', newLeftPosition, 'right:', rightPosition);
+  }
+
+  function onMouseUpAction() {
+    document.removeEventListener('mouseup', onMouseUpAction);
+    document.removeEventListener('mousemove', onMouseMoveAction);
+  }
+};

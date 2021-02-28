8a865206fcfa507cc6bdebc5997e88c1ecc6cf75
julietvt <freshitpt@gmail.com>
2021-02-28 12:15:49 +0200

    add menu

--- assets/js/task27.js
+++ assets/js/task27.js
@@ -2,12 +2,21 @@ const my_pattern = /\W/g;
 
 const inputTaskElem = document.querySelector('input[name="task"]');
 const createTaskButtonElement = document.getElementById('createTaskButton');
-
+inputTaskElem.oninput = onInputHandler;
+createTaskButtonElement.addEventListener('click', onCreateTaskButtonClick);
 let isValid = false;
 
-function f(e) {
+function onCreateTaskButtonClick(e) {
+  const taskInputElem = document.querySelector(
+    'input[placeholder="Enter your task"]'
+  );
+  taskInputElem.value = '';
+}
+
+function onInputHandler(e) {
   isValid = my_pattern.test(this.value);
   if (isValid) {
+    // this e.target
     this.classList.remove('invalidStyle');
     this.classList.add('validStyle');
   } else {
--- assets/js/task29.js
+++ assets/js/task29.js
@@ -5,6 +5,8 @@ document.getElementById('id1').onclick = function () {
   button_id2.hidden = true;
 };
 */
+
+/*
 let slider = document.querySelector('.slider');
 let thumb = document.querySelector('.thumb');
 thumb.onmousedown = function (event) {
@@ -29,3 +31,25 @@ thumb.onmousedown = function (event) {
     document.removeEventListener('mousemove', onMouseMoveAction);
   }
 };
+*/
+
+// home task
+/*
+Дан элемент div с id=elem
+- Добавьте ему класс www.
+- Удалите у него класс www.
+- Проверьте наличие у него класса www.
+- Добавьте содержимое в элемент (текст)
+Реализуйте событие: по нажатию на элемент (см задание 2) пользователь получит сообщение о нажатии
+*/
+
+let elem1 = document.getElementById('elem');
+elem1.classList.add('www');
+elem1.classList.remove('www');
+let isContain = elem1.classList.contains('www');
+console.log(isContain);
+elem1.innerText = 'Hello!';
+elem1.addEventListener('click', hello);
+function hello(event) {
+  alert('Element clicked');
+}
